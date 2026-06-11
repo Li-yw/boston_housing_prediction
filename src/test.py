e41f20eb5173a0d7568a34495ec2607f91579694
Li-yw <95966303+Li-yw@users.noreply.github.com>
2026-06-11 18:12:37 +0800

    Remove src/test.py

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-iris = load_iris()
-print(iris.data)
-print(iris.target)
-print(iris.target_names)
-print(iris.feature_names)
